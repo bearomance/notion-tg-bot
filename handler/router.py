ef54da4bb4737d7eb77ba0453bceafa64e4d3a60
bearomance <mingjinger@gmail.com>
2025-02-26 21:46:30 +0800

    update

--- handler/router.py
+++ handler/router.py
@@ -9,9 +9,8 @@ class MessageRouter:
         print('Handling update')
         self.update = update
         message = update["message"]['text']
-        chat_id = message["chat"]["id"]
-
         notion_service.record(message,  datetime.now().isoformat())
 
+        chat_id = update["message"]["chat"]["id"]
         await telegram_service.send_message(message, chat_id)
         return self
--- service/notion_service.py
+++ service/notion_service.py
@@ -8,11 +8,14 @@ from model.page import Page
 notion = Client(auth=NOTION_TOKEN)
 
 def record(name, date):
-    new_page = Page() \
-        .title("Name", name) \
-        .date("Date", date) \
-        .build()
-    notion.pages.create(parent={"database_id": NOTION_DATABASE_ID}, properties=new_page)
+    try:
+        new_page = Page() \
+            .title("Name", name) \
+            .date("Date", date) \
+            .build()
+        notion.pages.create(parent={"database_id": NOTION_DATABASE_ID}, properties=new_page)
+    except Exception as e:
+        print(f"notion.pages.create ERROR: {e}")
 
 if __name__ == "__main__":
     record("Hello, world!", datetime.now().isoformat())
