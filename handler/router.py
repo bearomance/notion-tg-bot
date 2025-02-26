a671409eef2074e4feefc73a39e82e952226447e
bearomance <mingjinger@gmail.com>
2025-02-26 21:34:35 +0800

    update

--- handler/router.py
+++ handler/router.py
@@ -1,17 +1,17 @@
 from service import notion_service, telegram_service
-
+from datetime import datetime
 
 class MessageRouter:
     def __init__(self):
         self.update = {}
 
-    def handle(self, update):
+    async def handle(self, update):
         print('Handling update')
         self.update = update
-
-        notion_service.record('test', '2022-06-28')
-
         message = update["message"]
         chat_id = message["chat"]["id"]
-        telegram_service.send_message(message['text'], chat_id)
+
+        notion_service.record(message,  datetime.now().isoformat())
+
+        await telegram_service.send_message(message['text'], chat_id)
         return self
