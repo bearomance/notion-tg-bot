1a6c68c6630d62c6eb5d0b2854facf2fea16c7f1
bearomance <mingjinger@gmail.com>
2025-02-28 11:27:59 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 import asyncio
 from handler.router import MessageRouter
+from service import telegram_service
 
 app = FastAPI()
 router = MessageRouter()
@@ -19,12 +20,13 @@ async def webhook(request: Request):
         print(f"Handle webhook ERROR: {e}")
     return {"status": "ok"}
 
-@app.post("/cron")
+@app.post("/api/cron")
 async def cron_job():
     try:
         # 在这里添加您想要每小时执行的任务
         print("Running hourly cron job")
         # 例如：await router.some_hourly_task()
+        telegram_service.send_message("Hello, World!")
         return {"status": "success", "message": "Cron job executed successfully"}
     except Exception as e:
         print(f"Cron job ERROR: {e}")
